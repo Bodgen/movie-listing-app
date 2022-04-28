1745202fdd4277f39a46eb1ff87de426c350f84a
Bohdan Garashchuk <bohdan.garashchuk@gmail.com>
2022-04-28 12:14:52 +0300

    fixed Rating button  position

--- src/UI/Card/Card.js
+++ src/UI/Card/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import RatingButton from "../RatingButton/RatingButton";
+import image from '../../assets/img/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1.png'
 
 const CardStyles = styled.div`
   width: 280px;
@@ -9,6 +10,7 @@ const CardStyles = styled.div`
   backdrop-filter: blur(80px);
   border-radius: 12px;
   color: #EBEEF5;
+  margin-bottom: 16px;
 `
 
 const CardContainer = styled.div`
@@ -21,7 +23,15 @@ const CardImage = styled.div`
   height: 400px;
   border-radius: 8px;
   margin-bottom: 24px;
-
+  img{
+    max-width: 256px;
+    
+  }
+  .rating{
+    position: absolute;
+    top: 8px;
+    left: 10px;
+  }
 `
 
 const Card = () => {
@@ -29,7 +39,10 @@ const Card = () => {
         <CardStyles>
             <CardContainer>
                 <CardImage>
-                    <RatingButton id='rating'/>
+                    <img src={image} alt=""/>
+                    <div className='rating'>
+                        <RatingButton/>
+                    </div>
                 </CardImage>
                 <h6>Black Window</h6>
             </CardContainer>
